19df395b2f9db32315056d0e1dec02c84845f548
noasspectt <noasspect@gmail.com>
2025-04-14 19:41:51 +0300

    index.js

--- index.js
+++ index.js
@@ -231,5 +231,18 @@ afk.checkAFKStatus(client);
 
 });
 
+// Sunucu oluşturma ve proje aktivitesi sağlama.
+const app = express();
+const port = 3000;
+
+// Web sunucu
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} numaralı bağlantı noktasında yürütülüyor.`);
+});
+
 // Botu başlatma
 client.login(process.env.token);
